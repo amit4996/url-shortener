f5bf4ba1c0e6fbd862501f15867ecd0cdc64ff7d
amit4996 <amitkumaroffic@gmail.com>
2026-01-20 13:25:51 +0530

    added about.html

--- src/main/java/com/dev/url_shortener/HomeController.java
+++ src/main/java/com/dev/url_shortener/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home(){
         return "index.html";
     }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about.html";
+    }
 }
